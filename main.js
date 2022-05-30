147fb992e8a6b75d5b72af9977f6ff3d690fdc8a
Jesus Vargas <jesusvarguer18@gmail.com>
2022-05-30 17:19:36 -0500

    Haciendo uso de Get por defecto

---
+++ main.js
@@ -0,0 +1,36 @@
+const API_URL_RANDOM = "https://api.thecatapi.com/v1/images/search?limit=2&api_key=7482df93-9803-4144-9948-76141732c177";
+const API_URL_FAVORITES = "https://api.thecatapi.com/v1/favourites?limit=2&api_key=7482df93-9803-4144-9948-76141732c177";
+
+const error = document.getElementById("error");
+
+async function loadRandomMichis() {
+  const res = await fetch(API_URL_RANDOM);
+  const data = await res.json();
+  console.log("Random")
+  console.log(data)
+
+  if(res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status;
+  } else {
+    const img1 = document.getElementById('img1');
+    const img2 = document.getElementById('img2');
+
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+  }
+}
+
+
+async function loadFavoritesMichis() {
+  const res = await fetch(API_URL_FAVORITES);
+  const data = await res.json();
+  console.log("Favoritos")
+  console.log(data)
+
+  if(res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+  }
+}
+
+loadRandomMichis();
+loadFavoritesMichis();
