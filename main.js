629e9ec7ab42b2783f338363bffe961cf8566c01
Jesus Vargas <jesusvarguer18@gmail.com>
2022-05-30 22:07:42 -0500

    Modificando el archivo js con nuevas interaccionesy botones nuevos en el archivos html

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 const API_URL_RANDOM = "https://api.thecatapi.com/v1/images/search?limit=2&api_key=7482df93-9803-4144-9948-76141732c177";
 const API_URL_FAVORITES = "https://api.thecatapi.com/v1/favourites?limit=2&api_key=7482df93-9803-4144-9948-76141732c177";
 
-const error = document.getElementById("error");
+const spanError = document.getElementById('error')
 
 async function loadRandomMichis() {
   const res = await fetch(API_URL_RANDOM);
   const data = await res.json();
-  console.log("Random")
+  console.log('Random')
   console.log(data)
 
-  if(res.status !== 200) {
+  if (res.status !== 200) {
     spanError.innerHTML = "Hubo un error: " + res.status;
   } else {
     const img1 = document.getElementById('img1');
@@ -20,17 +20,36 @@ async function loadRandomMichis() {
   }
 }
 
-
-async function loadFavoritesMichis() {
+async function loadFavouriteMichis() {
   const res = await fetch(API_URL_FAVORITES);
   const data = await res.json();
-  console.log("Favoritos")
+  console.log('Favoritos')
   console.log(data)
 
-  if(res.status !== 200) {
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+  }
+}
+
+async function saveFavouriteMichis() {
+  const res = await fetch(API_URL_FAVORITES, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      image_id: 'dje'
+    }),
+  });
+  const data = await res.json();
+
+  console.log('Save')
+  console.log(res)
+
+  if (res.status !== 200) {
     spanError.innerHTML = "Hubo un error: " + res.status + data.message;
   }
 }
 
 loadRandomMichis();
-loadFavoritesMichis();
+loadFavouriteMichis();
